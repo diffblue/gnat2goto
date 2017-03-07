8463ca38f6035154dcf1503602b3409d3f1b46c9
Piotr Trojanek <piotr.trojanek@altran.com>
2017-03-07 14:49:22 +0000

    cleanup in Convert_Uint_To_Binary
    Rewrite with more contracts and GNAT coding style. Precondition needs some
    review.

--- gnat2goto/driver/uint_to_binary.adb
+++ gnat2goto/driver/uint_to_binary.adb
@@ -1,4 +1,3 @@
-
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Ada.Strings; use Ada.Strings;
@@ -7,12 +6,19 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Types; use Types;
 
 package body Uint_To_Binary is
-   
+
    -- TODO: handle negative numbers
-   function Convert_Uint_To_Binary (Input : Uint; Width : Integer) return String is
-      
-      function Hex_To_Binary (Input : String) return String is 
-	 
+   function Convert_Uint_To_Binary
+     (Input : Uint;
+      Width : Positive)
+      return String
+   is
+      function Hex_To_Binary (Input : String) return String is
+
+         function Hex_Char_To_Integer (Ch : Character) return Integer
+         with Pre  => Ch in '0' .. '9' | 'A' .. 'F' | 'a' .. 'f',
+              Post => Hex_Char_To_Integer'Result in 0 .. 15;
+
 	 function Hex_Char_To_Integer (Ch : Character) return Integer is
 	 begin
 	    case Ch is
@@ -26,49 +32,47 @@ package body Uint_To_Binary is
 		  raise Program_Error;
 	    end case;
 	 end;
-	 
+
 	 Ret : Unbounded_String := To_Unbounded_String ("");
-	 Intval : Integer;
-	 Buf : String(1..7);
+	 Buf : String (1 .. 7);
 	 package IO is new Integer_IO (Integer);
 
       begin
-	 for I in Integer range Input'First .. Input'Last loop
-	    Intval := Hex_Char_To_Integer (Input (I));
-	    IO.Put (Item => Intval, Base => 2, To => Buf);
+	 for I of Input loop
+	    IO.Put (Item => Hex_Char_To_Integer (I), Base => 2, To => Buf);
 	    declare
-	       Trimmed : constant String := Trim (Buf, Side => Left);
-	       Padded_Str : String(1..4);
+	       Trimmed    : constant String := Trim (Buf, Side => Left);
+	       Padded_Str : String (1 .. 4);
 	    begin
-	       Move(Source => Trimmed (Trimmed'First + 2 .. Trimmed'Last - 1),
-		    Target => Padded_Str,
-		    Justify => Right,
-		    Pad => '0');
+	       Move (Source  => Trimmed (Trimmed'First + 2 .. Trimmed'Last - 1),
+		     Target  => Padded_Str,
+		     Justify => Right,
+		     Pad     => '0');
 	       Append (Ret, Padded_Str);
 	    end;
 	 end loop;
 	 return To_String (Ret);
       end;
-      
-      Positive_Input : Uint := Input;
-      
+
+      Nonnegative_Input : constant Uint :=
+        (if Input >= 0
+         then Input
+         else Input + 2 ** Width);
+
    begin
-      pragma Assert (Input < 2 ** Width);
-      pragma Assert (Input >= -(2 ** (Width - 1)));
-      if (Input < 0) then
-	 Positive_Input := Input + (2 ** Width);
-      end if;
       declare
-	 Unpadded_Hex : constant String := UI_Image (Positive_Input, Hex);
-	 Padded_Hex : String (1..(Width/4));	 
+	 Unpadded_Hex : constant String := UI_Image (Nonnegative_Input, Hex);
+	 Padded_Hex   : String (1 .. Width / 4);
       begin
 	 pragma Assert (Unpadded_Hex (Unpadded_Hex'First) = ' ');
-	 Move(Source => Unpadded_Hex (Unpadded_Hex'First + 1 .. Unpadded_Hex'Last),
-	      Target => Padded_Hex,
-	      Justify => Right,
-	      Pad => '0');
-	 return Hex_To_Binary (Padded_Hex);
+
+         Move (Source  => Unpadded_Hex (Unpadded_Hex'First + 1 .. Unpadded_Hex'Last),
+	       Target  => Padded_Hex,
+	       Justify => Right,
+	       Pad     => '0');
+
+         return Hex_To_Binary (Padded_Hex);
       end;
    end;
-   
+
 end Uint_To_Binary;
--- gnat2goto/driver/uint_to_binary.ads
+++ gnat2goto/driver/uint_to_binary.ads
@@ -1,8 +1,16 @@
-
-with Uintp; use Uintp; 
+with Types;
+with Uintp; use Uintp;
 
 package Uint_To_Binary is
-   
-   function Convert_Uint_To_Binary (Input : Uint; Width : Integer) return String;
-  
+
+   use type Types.Int;
+   --  For operators on Uint and Int
+
+   function Convert_Uint_To_Binary
+     (Input : Uint;
+      Width : Positive)
+      return String
+   with Pre => Input >= -(2 ** (Width - 1)) and then Input < 2 ** Width;
+   --  ???                                          "Input < 2 ** (Width - 1)"
+
 end;
