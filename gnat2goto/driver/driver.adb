bdc50030b6e4e133324baeea94560cb8e6e73524
Chris Smowton <chris@smowton.net>
2017-03-09 15:30:40 +0000

    Implement local variable symbol gathering

--- gnat2goto/driver/driver.adb
+++ gnat2goto/driver/driver.adb
@@ -27,15 +27,34 @@ with Irep_Schemata; use Irep_Schemata;
 with Symbol_Table_Info; use Symbol_Table_Info;
 with Gather_Irep_Symbols;
 with Iinfo; use Iinfo;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+with Ada.Text_IO; use Ada.Text_IO;
+with GNATCOLL.JSON; use GNATCOLL.JSON;
 
 package body Driver is
 
    procedure GNAT_To_Goto (GNAT_Root : Node_Id)
    is
       Program_Irep : constant Irep_Code_Block := Do_Compilation_Unit (GNAT_Root);
+      Void_Type : constant Irep_Void_Type := Make_Irep_Void_Type;
+      Start_Name : constant Unbounded_String := To_Unbounded_String ("_start");
+      Function_Type : Irep_Code_Type := Make_Irep_Code_Type;
       Local_Symbol_Table : Symbol_Table;
+      Function_Symbol : Symbol;
    begin
       Gather_Irep_Symbols.Gather (Local_Symbol_Table, Irep (Program_Irep));
+
+      -- For now, emit this as _start.
+      Function_Symbol.Name := Start_Name;
+      Function_Symbol.PrettyName := Start_Name;
+      Function_Symbol.BaseName := Start_Name;
+      Set_Return_Type (Function_Type, Irep (Void_Type));
+      Function_Symbol.SymType := Irep (Function_Type);
+      Function_Symbol.Value := Irep (Program_Irep);
+      Function_Symbol.Mode := To_Unbounded_String ("C");
+      Symbol_Maps.Insert (Local_Symbol_Table, Start_Name, Function_Symbol);
+
+      Put_Line (Create (SymbolTable2Json (Local_Symbol_Table)).Write);
    end GNAT_To_Goto;
 
    function Is_Back_End_Switch (Switch : String) return Boolean is
--- gnat2goto/driver/gather_irep_symbols.adb
+++ gnat2goto/driver/gather_irep_symbols.adb
@@ -1,9 +1,45 @@
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
 package body Gather_Irep_Symbols is
 
    procedure Gather (Table : in out Symbol_Table; Ir : Irep) is
    begin
-      null;
+      if (To_String(Ir.Id) = "symbol") then
+        declare
+           Id : constant Unbounded_String :=
+             Irep_Maps.Element (Ir.Named_Sub, To_Unbounded_String ("identifier")).Id;
+        begin
+           if not Symbol_Maps.Contains (Table, Id) then
+              declare
+                 Expr_Type : Irep :=
+                   Irep_Maps.Element (Ir.Named_Sub, To_Unbounded_String ("type")).all;
+                 New_Symbol : Symbol;
+              begin
+                 New_Symbol.SymType := Expr_Type;
+                 New_Symbol.Value := Trivial.Trivial_Irep ("nil");
+                 New_Symbol.Name := Id;
+                 New_Symbol.BaseName := Id;
+                 New_Symbol.PrettyName := Id;
+                 New_Symbol.Mode := To_Unbounded_String ("C");
+                 New_Symbol.IsStateVar := True;
+                 New_Symbol.IsThreadLocal := True;
+                 New_Symbol.IsLValue := True;
+                 Symbol_Maps.Insert (Table, Id, New_Symbol);
+              end;
+           end if;
+        end;
+      else
+         for Sub of Ir.Sub loop
+            Gather (Table, Sub.all);
+         end loop;
+         for Sub_Pair in Ir.Named_Sub.Iterate loop
+            Gather (Table, Irep_Maps.Element (Sub_Pair).all);
+         end loop;
+         for Sub_Pair in Ir.Comment.Iterate loop
+            Gather (Table, Irep_Maps.Element (Sub_Pair).all);
+         end loop;
+      end if;
    end;
 
 end Gather_Irep_Symbols;
