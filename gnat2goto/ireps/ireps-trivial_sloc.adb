c65ba437f931ada5f476742ae35b4b37f1ded757
Florian Schanda <florian.schanda@altran.com>
2017-03-24 13:25:50 +0000

    Make serialisation of source locations work.

--- gnat2goto/ireps/ireps-trivial_sloc.adb
+++ gnat2goto/ireps/ireps-trivial_sloc.adb
@@ -1,26 +1,24 @@
+with Sinput; use Sinput;
+
 separate (Ireps)
 function Trivial_Sloc (S : Source_Ptr) return JSON_Value
 is
-   pragma Unreferenced (S);
-begin
-   return V : constant JSON_Value := Create_Object do
-      V.Set_Field ("id", "source_location");
-      V.Set_Field ("sub", Empty_Array);
-      V.Set_Field ("comment", Create_Object);
-      declare
-         N : constant JSON_Value := Create_Object;
-      begin
-         N.Set_Field ("file", Trivial_String ("todo"));
-         --  N.Set_Field ("working_directory", Trivial_String (""));
-         N.Set_Field ("line", Trivial_Integer (42));
-         N.Set_Field ("column", Trivial_Integer (666));
-         --  N.Set_Field ("function", Trivial_String (""));
-         --  N.Set_Field ("property_id", Trivial_String (""));
-         --  N.Set_Field ("property_class", Trivial_String (""));
-         --  N.Set_Field ("comment", Trivial_String (""));
-         --  N.Set_Field ("java_bytecode_index", Trivial_String (""));
+   Source_Location : constant JSON_Value := Create_Object;
+   N               : constant JSON_Value := Create_Object;
 
-         V.Set_Field ("namedSub", N);
-      end;
-   end return;
+   SI : constant Source_File_Index := Get_Source_File_Index (S);
+   F  : constant File_Name_Type    := File_Name (SI);
+begin
+   Source_Location.Set_Field ("id", "source_location");
+
+   if S /= No_Location and S > Standard_Location then
+      N.Set_Field ("file", Trivial_String (Get_Name_String (F)));
+      N.Set_Field ("line",
+                   Trivial_Integer (Integer (Get_Logical_Line_Number (S))));
+      N.Set_Field ("column",
+                   Trivial_Integer (Integer (Get_Column_Number (S))));
+      Source_Location.Set_Field ("namedSub", N);
+   end if;
+
+   return Source_Location;
 end Trivial_Sloc;
