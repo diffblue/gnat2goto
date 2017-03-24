7e21223a75bbf26062a3c8633dfd11a76ca423dd
Florian Schanda <florian.schanda@altran.com>
2017-03-24 12:35:26 +0000

    Smaller output for source_location.

--- gnat2goto/ireps/ireps-trivial_sloc.adb
+++ gnat2goto/ireps/ireps-trivial_sloc.adb
@@ -11,14 +11,14 @@ begin
          N : constant JSON_Value := Create_Object;
       begin
          N.Set_Field ("file", Trivial_String ("todo"));
-         N.Set_Field ("working_directory", Trivial_String (""));
+         --  N.Set_Field ("working_directory", Trivial_String (""));
          N.Set_Field ("line", Trivial_Integer (42));
          N.Set_Field ("column", Trivial_Integer (666));
-         N.Set_Field ("function", Trivial_String (""));
-         N.Set_Field ("property_id", Trivial_String (""));
-         N.Set_Field ("property_class", Trivial_String (""));
-         N.Set_Field ("comment", Trivial_String (""));
-         N.Set_Field ("java_bytecode_index", Trivial_String (""));
+         --  N.Set_Field ("function", Trivial_String (""));
+         --  N.Set_Field ("property_id", Trivial_String (""));
+         --  N.Set_Field ("property_class", Trivial_String (""));
+         --  N.Set_Field ("comment", Trivial_String (""));
+         --  N.Set_Field ("java_bytecode_index", Trivial_String (""));
 
          V.Set_Field ("namedSub", N);
       end;
