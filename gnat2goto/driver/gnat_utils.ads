9f8392dc85d578c302af0dd5f33c1c75b2e29457
Piotr Trojanek <piotr.trojanek@altran.com>
2017-04-24 13:42:47 +0100

    move gnat2why utilities to separate file

---
+++ gnat2goto/driver/gnat_utils.ads
@@ -0,0 +1,20 @@
+with Atree; use Atree;
+with Sinfo; use Sinfo;
+with Types; use Types;
+
+package GNAT_Utils is
+
+   --  Utility routines temporarily copied from gnat2why, which perhaps should
+   --  be moved to GNAT (or some other place to avoid duplicated effort, but we
+   --  need to discuss this).
+   --
+   --  Intentially undocumented; see gnat2why originals for code
+   --
+   --  ??? fix license (Copyright AdaCore and Altran)
+
+   generic
+      with procedure Handle_Parameter (Formal : Entity_Id; Actual : Node_Id);
+   procedure Iterate_Call_Parameters (Call : Node_Id)
+   with Pre => Nkind (Call) in N_Subprogram_Call | N_Entry_Call_Statement;
+
+end GNAT_Utils;
--- gnat2goto/driver/tree_walk.adb
+++ gnat2goto/driver/tree_walk.adb
@@ -6,8 +6,9 @@ with Uintp;                 use Uintp;
 with Einfo;                 use Einfo;
 with Snames;                use Snames;
 
-with Uint_To_Binary;        use Uint_To_Binary;
 with Follow;                use Follow;
+with GNAT_Utils;            use GNAT_Utils;
+with Uint_To_Binary;        use Uint_To_Binary;
 
 package body Tree_Walk is
 
@@ -148,11 +149,6 @@ package body Tree_Walk is
    with Post => Kind (Process_Statement_List'Result) = I_Code_Block;
    --  Process list of statements or declarations
 
-   generic
-      with procedure Handle_Parameter (Formal : Entity_Id; Actual : Node_Id);
-   procedure Iterate_Call_Parameters (Call : Node_Id)
-   with Pre => Nkind (Call) in N_Subprogram_Call | N_Entry_Call_Statement;
-
    -------------------
    -- Make_Int_Type --
    -------------------
@@ -190,73 +186,6 @@ package body Tree_Walk is
 
    -- Borrowed from https://github.com/AdaCore/spark2014/blob/master/gnat2why/spark/spark_util.adb
 
-   -----------------------
-   -- Get_Called_Entity --
-   -----------------------
-
-   function Get_Called_Entity (N : Node_Id) return Entity_Id is
-      Nam : constant Node_Id := Name (N);
-
-   begin
-      return
-        Entity (case Nkind (Nam) is
-                   when N_Selected_Component => Selector_Name (Nam),
-                   when N_Indexed_Component  => Selector_Name (Prefix (Nam)),
-                   when others               => Nam);
-   end Get_Called_Entity;
-
-   -----------------------------
-   -- Iterate_Call_Parameters --
-   -----------------------------
-
-   procedure Iterate_Call_Parameters (Call : Node_Id)
-   is
-      Params     : constant List_Id := Parameter_Associations (Call);
-      Cur_Formal : Entity_Id := First_Entity (Get_Called_Entity (Call));
-      Cur_Actual : Node_Id := First (Params);
-      In_Named   : Boolean := False;
-   begin
-      --  We have to deal with named arguments, but the frontend has
-      --  done some work for us. All unnamed arguments come first and
-      --  are given as-is, while named arguments are wrapped into a
-      --  N_Parameter_Association. The field First_Named_Actual of the
-      --  function or procedure call points to the first named argument,
-      --  that should be inserted after the last unnamed one. Each
-      --  Named Actual then points to a Next_Named_Actual. These
-      --  pointers point directly to the actual, but Next_Named_Actual
-      --  pointers are attached to the N_Parameter_Association, so to
-      --  get the next actual from the current one, we need to follow
-      --  the Parent pointer.
-      --
-      --  The Boolean In_Named states how to obtain the next actual:
-      --  either follow the Next pointer, or the Next_Named_Actual of
-      --  the parent.
-      --  We start by updating the Cur_Actual and In_Named variables for
-      --  the first parameter.
-
-      if Nkind (Cur_Actual) = N_Parameter_Association then
-         In_Named := True;
-         Cur_Actual := First_Named_Actual (Call);
-      end if;
-
-      while Present (Cur_Formal) and then Present (Cur_Actual) loop
-         Handle_Parameter (Cur_Formal, Cur_Actual);
-         Cur_Formal := Next_Formal (Cur_Formal);
-
-         if In_Named then
-            Cur_Actual := Next_Named_Actual (Parent (Cur_Actual));
-         else
-            Next (Cur_Actual);
-
-            if Nkind (Cur_Actual) = N_Parameter_Association then
-               In_Named := True;
-               Cur_Actual := First_Named_Actual (Call);
-            end if;
-         end if;
-      end loop;
-   end Iterate_Call_Parameters;
-
-   -- End borrowed code
 
    -----------------------------
    -- Do_Assignment_Statement --
@@ -1086,6 +1015,10 @@ package body Tree_Walk is
 
       return Ret;
    end Do_Operator;
+
+   -----------------
+   -- Do_Constant --
+   -----------------
 
    function Do_Constant (N : Node_Id) return Irep is
       Ret           : constant Irep := New_Irep (I_Constant_Expr);
