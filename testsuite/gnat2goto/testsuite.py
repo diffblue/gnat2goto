4660a5d73b96608e90e6111e3f853f072eff7004
Chris Ryder <chris.ryder@diffblue.com>
2018-12-04 15:43:44 +0000

    Gather metrics for UOK test results and treat them as failures

--- testsuite/gnat2goto/testsuite.py
+++ testsuite/gnat2goto/testsuite.py
@@ -12,6 +12,7 @@ from gnatpython.mainloop import (MainLoop, add_mainloop_options,
                                  setup_result_dir)
 from gnatpython.testdriver import add_run_test_options
 from gnatpython.reports import ReportDiff
+from gnatpython.fileutils import (split_file)
 from glob import glob
 import os
 import sys
@@ -52,25 +53,39 @@ def main():
     if m.options.discs:
         discs += m.options.discs.split(',')
 
-    test_metrics = {'total': len(test_list)}
+    test_metrics = {'total': len(test_list), 'uok': 0}
 
-    collect_result = generate_collect_result(
+    # Generate a standard 'collect_result' function...
+    generated_collect_result = generate_collect_result(
         result_dir = m.options.output_dir,
         results_file = m.options.results_file,
         output_diff = m.options.view_diffs,
         metrics = test_metrics)
 
+    # ... and then wrap that generated 'collect_result' function in something
+    # that will also accumulate 'UOK' test results
+    def collect_result_including_uok(name, process, _job_info):
+        generated_collect_result(name, process, _job_info)
+        test_name = test_name = os.path.basename(name)
+        test_result = split_file(
+            m.options.output_dir + '/' + test_name + '.result',
+            ignore_errors=True)
+        if test_result:
+            test_status = test_result[0].split(':')[0]
+            if test_status == 'UOK':
+                test_metrics['uok'] += 1
+
     run_testcase = generate_run_testcase('run-test', discs, m.options)
 
-    MainLoop(test_list, run_testcase, collect_result, m.options.mainloop_jobs)
+    MainLoop(test_list, run_testcase, collect_result_including_uok, m.options.mainloop_jobs)
 
-    print "Summary: Ran %(run)s/%(total)s tests, with %(failed)s failed, %(crashed)s crashed." % test_metrics
+    print "Summary: Ran %(run)s/%(total)s tests, with %(failed)s failed, %(crashed)s crashed, %(uok)s unexpectedly passed." % test_metrics
 
     # Generate the report file
     ReportDiff(m.options.output_dir,
                m.options.old_output_dir).txt_image(m.options.report_file)
 
-    if test_metrics['failed'] > 0 or test_metrics['crashed'] > 0:
+    if test_metrics['failed'] > 0 or test_metrics['crashed'] > 0 or test_metrics['uok'] > 0:
         sys.exit(1)
 
 
