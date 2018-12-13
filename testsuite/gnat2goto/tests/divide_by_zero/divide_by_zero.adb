160443cb222403b0ef701b83a343c37e40c63ca1
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2018-12-13 09:53:56 +0000

    Add a test to show division by zero check is working now

---
+++ testsuite/gnat2goto/tests/divide_by_zero/divide_by_zero.adb
@@ -0,0 +1,7 @@
+procedure Divide_By_Zero is
+  A : Integer := 10;
+  B : Integer := 0;
+  C : Integer;
+begin
+  C := A / B;
+end;
---
+++ testsuite/gnat2goto/tests/divide_by_zero/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+prove("--div-by-zero-check")
