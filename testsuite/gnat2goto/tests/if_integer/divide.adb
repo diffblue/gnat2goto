4adb75326934624e3ca4a23fc74d2e90940bcbc4
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-12-17 17:24:02 +0000

    Enable if_integer test

--- testsuite/gnat2goto/tests/if_integer/divide.adb
+++ testsuite/gnat2goto/tests/if_integer/divide.adb
@@ -1,11 +1,17 @@
-procedure Divide (b : Integer) is
-  a : Integer := 1;
-  c : Integer;
-  d : Integer;
+procedure Divide (B : Integer; C : Integer) is
+--  A : Integer;
 begin
-  if a > 0 then
-     c := a / b;
-  else
-     d := a / b;
+  pragma Assert (B / 1 = B);
+  if B > 1 and then C > B then
+   pragma Assert (B / B = 1);
+   --  reachability check, should be false
+   pragma Assert (B < 0);
+
+   -- the following should also be true,
+   -- but cbmc will take a very long time to verify them
+   --  A := C / B;
+   --  pragma Assert (A > 0);
+   --  pragma Assert (A < C);
+   --  pragma Assert (C - A*B < B);
   end if;
 end;
