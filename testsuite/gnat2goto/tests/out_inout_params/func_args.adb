285dcdf9be0b460f2c0d2e1d44ec0a2ba97ed964
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-12-17 17:56:07 +0000

    Enable out_inout_params test

--- testsuite/gnat2goto/tests/out_inout_params/func_args.adb
+++ testsuite/gnat2goto/tests/out_inout_params/func_args.adb
@@ -1,12 +1,19 @@
 procedure Func_Args is
-   a : Integer := 1;
-   b : Integer := 2;
-   c : Integer;
-   function Assign2 (D : in out Integer; E : out Integer) return Integer is begin
+   A : Integer := 1;
+   B : Integer := 2;
+   C : Integer;
+
+   function Assign2 (D : in out Integer; E : out Integer) return Integer is
+   begin
       D := D + 1;
-      E := D;
-      return D;
+      E := D + 1;
+      return E + 1;
    end;
+
 begin
    C := Assign2 (A, B);
+   pragma Assert (A = 2);
+   pragma Assert (B = 3);
+   pragma Assert (C = 4);
+   pragma Assert (A > B);
 end;
