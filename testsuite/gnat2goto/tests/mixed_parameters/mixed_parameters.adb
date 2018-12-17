150ba3e8db5a6693e9a554c588b5afdbe7605205
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2018-12-17 17:51:57 +0000

    Enabled mixed_parameters test

--- testsuite/gnat2goto/tests/mixed_parameters/mixed_parameters.adb
+++ testsuite/gnat2goto/tests/mixed_parameters/mixed_parameters.adb
@@ -1,13 +1,19 @@
 procedure Mixed_Parameters is
-   a : Integer := 1;
-   b : Integer := 2;
-   c : Integer;
-   function Mixed (D : Integer; E : Integer) return Integer is begin
-      return D - E;
+   C : Integer;
+
+   function Subtract (Minuend : Integer; Subtrahend : Integer) return Integer is begin
+      return Minuend - Subtrahend;
    end;
+
 begin
-   C := Mixed (A, B);
-   C := Mixed (D => C, E => B);
-   C := Mixed (E => C, D => B);
-   C := Mixed (A, E => C);
+   C := Subtract (2, 1);
+   pragma Assert (C = 1);
+   C := Subtract (Minuend => C, Subtrahend => 1);
+   pragma Assert (C = 0);
+   C := Subtract (Subtrahend => C, Minuend => 5);
+   pragma Assert (C = 5);
+   C := Subtract (7, Subtrahend => C);
+   pragma Assert (C = 2);
+   C := Subtract (C, C);
+   pragma Assert (C /= 0);
 end;
