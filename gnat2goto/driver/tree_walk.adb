bbb6646848efa3de4dc876120d8aa2d63042fa98
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-01-18 15:25:28 +0000

    Rewrite get_array_dup_function
    Use wrappers for calling malloc, param creation, etc.

--- gnat2goto/driver/tree_walk.adb
+++ gnat2goto/driver/tree_walk.adb
@@ -4455,69 +4455,100 @@ package body Tree_Walk is
          return Array_Dup_Maps.Element (Map_Cursor);
       end if;
 
-      --  Create a new duplicator function:
+      --  Build a function:
+      --  elem_type* dup_array(elem_type* ptr, int len) {
+      --    elem_type* new_array = alloc(sizeof(elem_type) * len);
+      --    copy_array(new_array, ptr, len);
+      --    return new_array;
+      --  }
       declare
-
-         Func_Type : constant Irep := New_Irep (I_Code_Type);
-         Func_Args : constant Irep := New_Irep (I_Parameter_List);
-         Ptr_Arg : constant Irep := New_Irep (I_Code_Parameter);
-         Ptr_Type : constant Irep :=
-           Make_Pointer_Type (Do_Type_Reference (Element_Type));
-         Len_Arg : constant Irep := New_Irep (I_Code_Parameter);
-         Len_Type : constant Irep := Do_Type_Reference (Index_Type);
-         Func_Symbol : Symbol;
+         Func_Params : constant Irep := New_Irep (I_Parameter_List);
          Map_Size_Str : constant String :=
            Integer'Image (Integer (Array_Dup_Map.Length));
          Func_Name : constant String :=
            "__ada_dup_array" & Map_Size_Str (2 .. Map_Size_Str'Last);
+         Ptr_Type : constant Irep :=
+           Make_Pointer_Type (Do_Type_Reference (Element_Type));
+         Func_Type : constant Irep :=
+           Make_Code_Type (Parameters  => Func_Params,
+                           --  the parameters are build later
+                           Ellipsis    => False,
+                           Return_Type => Ptr_Type,
+                           Inlined     => False,
+                           Knr         => False);
+         Source_Loc : constant Source_Ptr := Sloc (Element_Type);
+         Ptr_Param : constant Irep :=
+           Create_Fun_Parameter (Fun_Name        => Func_Name,
+                                 Param_Name      => "ptr",
+                                 Param_Type      => Ptr_Type,
+                                 Param_List      => Func_Params,
+                                 A_Symbol_Table  => Global_Symbol_Table,
+                                 Source_Location => Source_Loc);
+         Len_Type : constant Irep := Do_Type_Reference (Index_Type);
+         Len_Param : constant Irep :=
+           Create_Fun_Parameter (Fun_Name        => Func_Name,
+                                 Param_Name      => "len",
+                                 Param_Type      => Len_Type,
+                                 Param_List      => Func_Params,
+                                 A_Symbol_Table  => Global_Symbol_Table,
+                                 Source_Location => Source_Loc);
+
+         Func_Symbol : Symbol;
          Array_Copy : constant Irep :=
            Fresh_Var_Symbol_Expr (Ptr_Type, "new_array");
-         Array_Alloc : constant Irep :=
-           New_Irep (I_Side_Effect_Expr_Cpp_New_Array);
-         Body_Block : constant Irep := New_Irep (I_Code_Block);
-         Call_Inst : constant Irep := New_Irep (I_Code_Function_Call);
+         Body_Block : constant Irep := Make_Code_Block (Source_Loc);
          Call_Args : constant Irep := New_Irep (I_Argument_List);
-         Return_Inst : constant Irep := New_Irep (I_Code_Return);
+         Lhs_fun_call : constant Irep :=
+           Fresh_Var_Symbol_Expr (Do_Type_Reference (Element_Type),
+                                  "array_dup_fun_lhs");
+         Call_Inst : constant Irep :=
+           Make_Code_Function_Call (Arguments       => Call_Args,
+                                    --  the argument are appended later
+                                    I_Function      =>
+                                      Get_Array_Copy_Function (
+                                        Element_Type,
+                                        Element_Type,
+                                        Index_Type),
+                                    Lhs             => Lhs_fun_call,
+                                    Source_Location => Source_Loc);
+         Return_Inst : constant Irep :=
+           Make_Code_Return (Return_Value    => Array_Copy,
+                             Source_Location => Source_Loc);
 
+         --  new variables for malloc allocation
+         Member_Size : constant Irep :=
+           Make_Constant_Expr (Source_Location => Source_Loc,
+                               I_Type          => Len_Type,
+                               Range_Check     => False,
+                               Value           =>
+                    Convert_Uint_To_Hex (Value     => Esize (Element_Type) / 8,
+                                         Bit_Width => 64));
       begin
-
-         --  Create type (element_type*, index_type) -> element_type*
-         Set_Type (Ptr_Arg, Ptr_Type);
-         Set_Identifier (Ptr_Arg, Func_Name & "::ptr");
-         Set_Base_Name (Ptr_Arg, "ptr");
-         Set_Type (Len_Arg, Len_Type);
-         Set_Identifier (Len_Arg, Func_Name & "::len");
-         Set_Base_Name (Len_Arg, "len");
-         Append_Parameter (Func_Args, Ptr_Arg);
-         Append_Parameter (Func_Args, Len_Arg);
-         Set_Parameters (Func_Type, Func_Args);
-         Set_Return_Type (Func_Type, Ptr_Type);
-
          --  Create body (allocate and then call array_copy)
-         Set_Size (Array_Alloc, Param_Symbol (Len_Arg));
-         Set_Type (Array_Alloc, Ptr_Type);
-         Append_Declare_And_Init (Array_Copy, Array_Alloc, Body_Block, 0);
-         Append_Argument (Call_Args, Array_Copy);
-         Append_Argument (Call_Args, Param_Symbol (Ptr_Arg));
-         Append_Argument (Call_Args, Param_Symbol (Len_Arg));
-         Set_Arguments (Call_Inst, Call_Args);
-         Set_Function (Call_Inst,
-                       Get_Array_Copy_Function (Element_Type,
-                                                Element_Type,
-                                                Index_Type));
-         Append_Op (Body_Block, Call_Inst);
+         Append_Declare_And_Init (
+           Array_Copy,
+           Make_Op_Typecast (
+             Op0 =>  Make_Malloc_Function_Call_Expr (
+               Make_Op_Mul (
+                 Lhs => Member_Size,
+                 Rhs => Param_Symbol (Len_Param),
+                 I_Type => Len_Type,
+                 Source_Location => Source_Loc)),
+                 I_Type => Ptr_Type,
+             Source_Location => Source_Loc),
+                                  Body_Block, 0);
 
-         Set_Return_Value (Return_Inst, Array_Copy);
+         Append_Argument (Call_Args, Array_Copy);
+         Append_Argument (Call_Args, Param_Symbol (Ptr_Param));
+         Append_Argument (Call_Args, Param_Symbol (Len_Param));
+         Append_Op (Body_Block, Call_Inst);
          Append_Op (Body_Block, Return_Inst);
 
-         --  Make function symbol:
-         Func_Symbol.SymType := Func_Type;
-         Func_Symbol.Name := Intern (Func_Name);
-         Func_Symbol.PrettyName := Func_Symbol.Name;
-         Func_Symbol.BaseName := Func_Symbol.Name;
-         Func_Symbol.Mode := Intern ("C");
-         Func_Symbol.Value := Body_Block;
-         Global_Symbol_Table.Insert (Intern (Func_Name), Func_Symbol);
+         Func_Symbol :=
+           New_Function_Symbol_Entry (Name           => Func_Name,
+                                      Symbol_Type    => Func_Type,
+                                      Value          => Body_Block,
+                                      A_Symbol_Table => Global_Symbol_Table);
 
          --  Record it for the future:
          Array_Dup_Map.Replace_Element (Map_Cursor, Symbol_Expr (Func_Symbol));
