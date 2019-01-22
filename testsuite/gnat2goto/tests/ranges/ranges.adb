8ad88137a57a42b6ea22d1d5cc4e3dfc4e7baa60
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-01-22 16:01:51 +0000

    Test expansion for range check

--- testsuite/gnat2goto/tests/ranges/ranges.adb
+++ testsuite/gnat2goto/tests/ranges/ranges.adb
@@ -1,11 +1,12 @@
 procedure Ranges is
-   type Int1 is range 1 .. 10;
-   type Int2 is new Int1 range 2 .. 9;
-   subtype Int3 is Int1 range 3 .. 8;
-   A : Int1 := 5;
-   B : Int2;
-   C : Int3;
+   type Int_Big is range 1 .. 10000000;
+   type Int_Small is range -8 .. 8;
+   A : Int_Big := 5;
+   B : Int_Big := 900000;
+   C : Int_Small;
 begin
-   B := Int2(A);
-   C := A;
+   C := Int_Small (A);
+   C := Int_Small (B);
+   C := -2;
+   A := Int_Big (C);
 end;
