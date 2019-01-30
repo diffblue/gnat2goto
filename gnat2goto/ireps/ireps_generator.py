0a27e173bba48385411632ed3fcbb6bfdc90442d
Chris Ryder <chris.ryder@diffblue.com>
2019-01-30 14:23:26 +0000

    Only output JSON objects/arrays if they are non-empty.

--- gnat2goto/ireps/ireps_generator.py
+++ gnat2goto/ireps/ireps_generator.py
@@ -1910,8 +1910,14 @@ class IrepsGenerator(object):
             self.to_json_single_schema_name(b, sn)
         write(b, "end case;")
         write(b, "")
-        write(b, 'V.Set_Field ("sub",      Sub);')
-        write(b, 'V.Set_Field ("namedSub", Named_Sub);')
+        write(b, "if not Is_Empty (Sub) then")
+        with indent(b):
+            write(b, 'V.Set_Field ("sub",      Sub);')
+        write(b, "end if;")
+        write(b, "if not Is_Empty (Named_Sub) then")
+        with indent(b):
+            write(b, 'V.Set_Field ("namedSub", Named_Sub);')
+        write(b, "end if;")
         manual_outdent(b)
         write(b, "end;")
         write(b, "return V;")
