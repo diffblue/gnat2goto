17daa39a8cdf969ad8f5caad6662402778959ef1
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-02-14 08:23:35 +0000

    Fix irep traversal functions
    Now new lists are only created if a modification occurred.

--- gnat2goto/ireps/ireps-follow_irep.adb
+++ gnat2goto/ireps/ireps-follow_irep.adb
@@ -1,3 +1,11 @@
+-------------------------------------------------------------------------------
+--  Separate definition for following the symbol types throughout Irep_Lists(s)
+--  Implementation is adopted from ireps-to_json.adb
+--  The rationale is to create a new list only when a new irep was created
+--  by the respective Follow_Irep. First, that sounds efficient and second
+--  Ireps were disappearing when new list was created (even if it was filled
+--  with the same ireps).
+-------------------------------------------------------------------------------
 separate (Ireps)
 function Follow_Irep (L : Irep_List;
                      Follow_Symbol : not null access function (Symbol_I : Irep)
@@ -5,17 +13,33 @@ function Follow_Irep (L : Irep_List;
 is
    The_List : Irep_List_Node;
    Ptr      : Internal_Irep_List;
+   Arr      : constant Irep_List := New_List;
+   Modified : Boolean := False;
 begin
-   return Arr : constant Irep_List := New_List do
-      if L /= 0 then
-         The_List := Irep_List_Table.Table (To_Internal_List (L));
-         Ptr      := To_Internal_List (Irep_List (The_List.A));
-         while Ptr /= 0 loop
-            Append (Arr,
-                    Follow_Irep (Irep (Irep_List_Table.Table (Ptr).A),
-                                 Follow_Symbol));
+   if L /= 0 then
+      The_List := Irep_List_Table.Table (To_Internal_List (L));
+      Ptr      := To_Internal_List (Irep_List (The_List.A));
+      while Ptr /= 0 loop
+         declare
+            Orig_Irep : constant Irep := Irep (Irep_List_Table.Table (Ptr).A);
+            New_Irep  : constant Irep := Follow_Irep (Orig_Irep,
+                                                      Follow_Symbol);
+         begin
+            if Orig_Irep /= New_Irep then
+               Append (Arr, New_Irep);
+               Modified := True;
+            else
+               Append (Arr, Orig_Irep);
+            end if;
             Ptr := Irep_List_Table.Table (Ptr).B;
-         end loop;
-      end if;
-   end return;
+         end;
+      end loop;
+   end if;
+
+   pragma Assert (L'Size = Arr'Size);
+   if Modified then
+      return Arr;
+   else
+      return L;
+   end if;
 end Follow_Irep;
--- gnat2goto/ireps/ireps-remove_bounds.adb
+++ gnat2goto/ireps/ireps-remove_bounds.adb
@@ -1,18 +1,40 @@
+-------------------------------------------------------------------------------
+--  Separate definition for removing type bounds throughout Irep_Lists(s)
+--  Implementation is adopted from ireps-to_json.adb
+--  The rationale is to create a new list only when a new irep was created
+--  by the respective Remove_Bounds.
+-------------------------------------------------------------------------------
 separate (Ireps)
 function Remove_Bounds (L : Irep_List) return Irep_List
 is
    The_List : Irep_List_Node;
    Ptr      : Internal_Irep_List;
+   Arr      : constant Irep_List := New_List;
+   Modified : Boolean := False;
 begin
-   return Arr : constant Irep_List := New_List do
-      if L /= 0 then
-         The_List := Irep_List_Table.Table (To_Internal_List (L));
-         Ptr      := To_Internal_List (Irep_List (The_List.A));
-         while Ptr /= 0 loop
-            Append (Arr,
-                    Remove_Bounds (Irep (Irep_List_Table.Table (Ptr).A)));
+   if L /= 0 then
+      The_List := Irep_List_Table.Table (To_Internal_List (L));
+      Ptr      := To_Internal_List (Irep_List (The_List.A));
+      while Ptr /= 0 loop
+         declare
+            Orig_Irep : constant Irep := Irep (Irep_List_Table.Table (Ptr).A);
+            New_Irep  : constant Irep := Remove_Bounds (Orig_Irep);
+         begin
+            if Orig_Irep /= New_Irep then
+               Append (Arr, New_Irep);
+               Modified := True;
+            else
+               Append (Arr, Orig_Irep);
+            end if;
             Ptr := Irep_List_Table.Table (Ptr).B;
-         end loop;
-      end if;
-   end return;
+         end;
+      end loop;
+   end if;
+
+   pragma Assert (L'Size = Arr'Size);
+   if Modified then
+      return Arr;
+   else
+      return L;
+   end if;
 end Remove_Bounds;
