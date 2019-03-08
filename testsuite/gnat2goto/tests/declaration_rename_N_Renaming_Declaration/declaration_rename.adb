392c737a9b1d11e1bf4173258de215bd467ce2d8
Sonny Martin <sonny.martin@diffblue.com>
2019-03-08 12:48:06 +0000

    Remove warning of unhandled node.
    Also adds further example to renames test.

--- testsuite/gnat2goto/tests/declaration_rename_N_Renaming_Declaration/declaration_rename.adb
+++ testsuite/gnat2goto/tests/declaration_rename_N_Renaming_Declaration/declaration_rename.adb
@@ -1,10 +1,13 @@
 procedure Declaration_Rename is
    A : Integer := 1;
    B : Integer;
+   New_Integer_With_Complicated_Name : Integer := 11;
+   Simply_Named_Int : Integer renames New_Integer_With_Complicated_Name;
    function My_Plus(Left, Right : Integer) return Integer renames "+";
 begin
    A := A + 1;
    B := A + 1;
    pragma Assert (B=3);
+   pragma Assert (Simply_Named_Int=11);
    pragma Assert (My_Plus(A,B)=5);
 end Declaration_Rename;
