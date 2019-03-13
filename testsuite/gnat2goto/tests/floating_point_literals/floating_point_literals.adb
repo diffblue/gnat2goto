e94e105ea3503e728b4da573559b718dd90bc4db
Petr Bauch <petr.bauch@DB0047.ox.diffblue.com>
2019-03-13 08:15:25 +0000

    Modify current test to demonstrate bug
    The conversion from real to ieee (in hex) is incorrect wrt. to (at least) the
    decimal part. In the new test both 1.1 and 1.2 are translated into 1.8f.

--- testsuite/gnat2goto/tests/floating_point_literals/floating_point_literals.adb
+++ testsuite/gnat2goto/tests/floating_point_literals/floating_point_literals.adb
@@ -2,9 +2,11 @@ procedure Floating_Point_Literals is
   X : Float := 1.0;
   Y : Float := 1.0;
   Z : Float := 3.0;
+  W : Float := 1.1;
 begin
   pragma Assert (X > Z);
   pragma Assert (X < Z);
   pragma Assert (X + Y = Z);
   pragma Assert (X + Y < Z);
+  pragma Assert (W < 1.2);
 end Floating_Point_Literals;
