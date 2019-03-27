59f7bdb7c536a52d968d53e90f1c92fba8ae133d
Sonny Martin <sonny.martin@diffblue.com>
2019-03-27 10:28:16 +0000

    tests for newly found array declaration failure modes

---
+++ testsuite/gnat2goto/tests/arrays_multidimensional_type_declaration_only/arrays_multidimensional_type_declaration_only.adb
@@ -0,0 +1,6 @@
+procedure Arrays_Multidimensional_Type_Declaration_Only is
+   type AA1 is array (0..2, 0..2) of Integer;
+   type AA2 is array (Integer range 0..2, Integer range 0..2) of Integer;
+begin
+   null;
+end Arrays_Multidimensional_Type_Declaration_Only;
---
+++ testsuite/gnat2goto/tests/arrays_multidimensional_type_instantiation_failure/arrays_multidimensional_type_instantiation_failure.adb
@@ -0,0 +1,6 @@
+procedure Arrays_Multidimensional_Type_Instantiation_Failure is
+   type AA1 is array (0..2, 0..2) of Integer;
+   AA1_One : AA1;
+begin
+   null;
+end Arrays_Multidimensional_Type_Instantiation_Failure;
