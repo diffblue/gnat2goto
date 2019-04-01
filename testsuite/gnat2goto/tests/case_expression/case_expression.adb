46a707c8ddda78494a3e4b47e6ecd50ed77d0026
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2019-04-01 10:17:51 +0100

    Convert commits to use functions that return values to avoid constant folding.

--- testsuite/gnat2goto/tests/case_expression/case_expression.adb
+++ testsuite/gnat2goto/tests/case_expression/case_expression.adb
@@ -2,15 +2,23 @@ procedure Case_Expression is
   type Weekday is (Mon, Tue, Wed, Thu, Fri, Sat, Sun);
   Day : Weekday := Tue;
   Hours : Integer;
+  --  We need this to avoid the front-end's
+  --  constant folding mess up with the validity
+  --  of this test.
+  function Nested_Case_Expr (WDay : Weekday) 
+    return Integer is
+  begin
+     return (case WDay is
+              when Mon => 1,
+              when Tue => 2,
+              when Wed => 3,
+              when Thu => 4,
+              when Fri => 5,
+              when Sat => 6,
+              when Sun => 7);
+  end Nested_Case_Expr;
 begin
-  Hours := (case Day is
-            when Mon => 1,
-            when Tue => 2,
-            when Wed => 3,
-            when Thu => 4,
-            when Fri => 5,
-            when Sat => 6,
-            when Sun => 7);
+  Hours := Nested_Case_Expr (Day);
   pragma Assert (Hours = 2);
   pragma Assert (Hours /= 1);
 end Case_Expression;
