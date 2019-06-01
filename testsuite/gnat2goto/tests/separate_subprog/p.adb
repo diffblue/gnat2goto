360d66673d58d398a09d1bea0673a7846fa96c61
Trevor Jennings <TrevorJennings@bcs.org>
2019-06-01 18:19:19 +0100

    Add test case for handling subprogram_body_stub

---
+++ testsuite/gnat2goto/tests/separate_subprog/p.adb
@@ -0,0 +1,15 @@
+--  Use *.asu file extension for a subunit so that it is not included as a
+--  a top level unit to be analysed using by the regression test system.
+--  The gnat front-end will automatically analyse the subunit when it
+--  encounters the sybprogram_body_stub.
+pragma Source_File_Name (
+			 Subunit_File_Name  => "*.asu",
+			Dot_Replacement => "-");
+
+procedure P (X : in out integer) is
+   procedure Inc (N : in out Integer) is separate;
+   Old_X : constant Integer := X;
+begin
+   Inc (X);
+   pragma Assert (X = Old_X + 1);
+end P;
