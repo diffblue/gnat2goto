e8a879267b277c6a53def4c44cd8a18011099f7a
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2020-01-21 15:50:26 +0000

    Refine tests to one crashing and one demonstrating fix

--- testsuite/gnat2goto/tests/object_declaration_statement_bounds_crashing/object_declaration_statement_bounds_crashing.adb
+++ testsuite/gnat2goto/tests/object_declaration_statement_bounds_crashing/object_declaration_statement_bounds_crashing.adb
@@ -1,11 +1,11 @@
-procedure Object_Declaration_Statement is
+procedure Object_Declaration_Statement_Bounds_Crashing is
   procedure Callit (A : Integer) is
     -- Base Index type
     type Range_Type is range 0 .. 79;
     Index : Range_Type := 0;
   begin
     if A > 10 then
-      for I in Range_Type range (Index + 1) .. 10
+      for I in Range_Type range (Index + 1) .. Range_Type'Last
       loop
         pragma Assert (I < 70);
       end loop;
@@ -15,4 +15,4 @@ procedure Object_Declaration_Statement is
   end Callit;
 begin
   Callit(12);
-end Object_Declaration_Statement;
+end Object_Declaration_Statement_Bounds_Crashing;
