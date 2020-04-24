1c812415991e9d00edd7071cc0aab8e71279c0f3
Trevor Jennings <TrevorJennings@bcs.org>
2020-04-24 16:30:55 +0100

    Add a test for library-level generic instantiations.

---
+++ testsuite/gnat2goto/tests/do_not_generate_json_for_generic_bodies/gen_pack.adb
@@ -0,0 +1,12 @@
+package body Gen_Pack is
+
+   -------
+   -- G --
+   -------
+
+   function G (V : T) return T is
+   begin
+      return V;
+   end G;
+
+end Gen_Pack;
---
+++ testsuite/gnat2goto/tests/do_not_generate_json_for_generic_bodies/test.py
@@ -0,0 +1,3 @@
+from test_support import *
+
+prove(main='main.adb', specs=['my_pack.ads'])
