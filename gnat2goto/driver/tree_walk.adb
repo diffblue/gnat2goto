a721a07ff20712edee9c724aa72efa69064fd2d4
Trevor Jennings <trevorjennings@bcs.org>
2020-05-19 15:48:54 +0100

    Support implicit dereferencing.

--- gnat2goto/driver/tree_walk.adb
+++ gnat2goto/driver/tree_walk.adb
@@ -1777,6 +1777,10 @@ package body Tree_Walk is
       Func_Symbol  : Symbol;
 
    begin
+      --  It seems as though an N_Explicit_Drereference is placed in the tree
+      --  even when the function call is an implicit dereference.
+      --  Hence, implicit dereferences do not have to be seperately handled,
+      --  they are handled as explicit dereferences.
       if Nkind (Name (N)) = N_Explicit_Dereference then
          declare
             Fun_Type : constant Irep :=
@@ -4203,6 +4207,10 @@ package body Tree_Walk is
    function Do_Procedure_Call_Statement (N : Node_Id) return Irep
    is
    begin
+      --  It seems as though an N_Explicit_Drereference is placed in the tree
+      --  even when the procedure call is an implicit dereference.
+      --  Hence, implicit dereferences do not have to be seperately handled,
+      --  they are handled as explicit dereferences.
       if Nkind (Name (N)) = N_Explicit_Dereference then
          declare
             Fun_Type : constant Irep :=
@@ -4579,7 +4587,27 @@ package body Tree_Walk is
    ---------------------------
 
    function Do_Selected_Component (N : Node_Id) return Irep is
-      Root           : constant Irep := Do_Expression (Prefix (N));
+      --  The prefix to a selected component may be an access to an
+      --  record object which must be implicitly dereferenced.
+      The_Prefix        : constant Node_Id := Prefix (N);
+      Prefix_Etype      : constant Node_Id := Etype (The_Prefix);
+      Prefix_Irep       : constant Irep := Do_Expression (The_Prefix);
+      Is_Implicit_Deref : constant Boolean := Is_Access_Type (Prefix_Etype);
+      Resolved_Type     : constant Irep :=
+        (if Is_Implicit_Deref then
+            Do_Type_Reference (Designated_Type (Prefix_Etype))
+         else
+            Do_Type_Reference (Prefix_Etype));
+      Root            : constant Irep :=
+        (if Is_Implicit_Deref then
+            Make_Dereference_Expr
+           (I_Type => Resolved_Type,
+            Object => Prefix_Irep,
+            Source_Location => Get_Source_Location (N))
+         else
+            Prefix_Irep);
+
+      --  Where required the prefix has been implicitly dereferenced.
       Component      : constant Entity_Id := Entity (Selector_Name (N));
 
       --  Example:
