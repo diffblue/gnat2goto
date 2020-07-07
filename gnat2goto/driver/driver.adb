c692df70a50b9586bce73e45598892315466ec06
Trevor Jennings <trevorjennings@bcs.org>
2020-07-07 16:29:40 +0100

    Predefined Natural and Positive are subtypes of Integer.
    They are now repesented as Bounded_Signedbv type with lower bounds
    of 0 for Natural and 1 for positive.

--- gnat2goto/driver/driver.adb
+++ gnat2goto/driver/driver.adb
@@ -44,6 +44,7 @@ with Namet;                 use Namet;
 with Lib;                   use Lib;
 with GNAT_Utils;            use GNAT_Utils;
 with GOTO_Utils;            use GOTO_Utils;
+with Range_Check;           use Range_Check;
 
 with GNAT2GOTO.Options;
 
@@ -616,8 +617,29 @@ package body Driver is
 
       function Translate_Signed_Type (Type_Node : Node_Id)
                                      return Irep
-      is (Make_Signedbv_Type
-            (Width => Get_Bv_Width (Type_Node)));
+      is
+      begin
+         if Type_Node in Standard_Natural | Standard_Positive then
+            --  It is a bounded integer subtype
+            declare
+               Lower_Bound  : constant Uint :=
+                 (if Type_Node = Standard_Natural then Uint_0 else Uint_1);
+               Higher_Bound : constant Uint :=
+                Intval (Type_High_Bound (Standard_Integer));
+            begin
+               return Make_Bounded_Signedbv_Type
+                       (Width       => Get_Bv_Width (Type_Node),
+                        Lower_Bound =>
+                          Store_Nat_Bound (Bound_Type_Nat (Lower_Bound)),
+                        Upper_Bound =>
+                          Store_Nat_Bound (Bound_Type_Nat (Higher_Bound)));
+            end;
+         else
+            --  It is an unbounded integer type
+            return Make_Signedbv_Type
+              (Width => Get_Bv_Width (Type_Node));
+         end if;
+      end Translate_Signed_Type;
 
       function Translate_Enum_Type (Type_Node : Node_Id)
                                    return Irep
