c072912657e3a4069970d0bb00d435822d806ebf
Simon Buist <simon.buist@gmail.com>
2021-01-15 13:28:12 +0000

    added signed integer ranges

--- testsuite/gnat2goto/tests/nonliteral_bound/nonliterals.adb
+++ testsuite/gnat2goto/tests/nonliteral_bound/nonliterals.adb
@@ -3,9 +3,17 @@ procedure Nonliterals is
 
    Actual1 : Range1 := 11;
 
+   subtype Range2 is Integer range -10 .. 20;
+
+   Actual2 : Range2 := -5;
+
    Result : Integer;
 begin
    Result := Range1'First + Range1'Last + Actual1;
 
    pragma Assert (Result = 41);
+
+   Result := Range2'First + Range2'Last + Actual2;
+
+   pragma Assert (Result = 5);
 end Nonliterals;
